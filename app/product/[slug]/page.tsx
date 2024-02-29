aa89da86bf11549a23ce13700001e4b9fdbb3cd7
Yurii Matvii <tahkict.yup4ik@gmail.com>
2024-02-29 12:51:13 +0200

    Update page.tsx

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { detailedProduct } from "@/app/interface";
 import { client } from "@/app/lib/santity";
 import AddToBag from "@/components/AddToBag";
+import CheckoutNow from "@/components/CheckoutNow";
 import ImageGallery from "@/components/ImageGallery";
 import { Button } from "@/components/ui/button";
 import { Star, Truck } from "lucide-react";
@@ -74,7 +75,15 @@ const ProductPage = async ({ params }: { params: { slug: string } }) => {
                 key={data._id}
                 price_id={data.price_id}
               />
-              <Button variant={"secondary"}>Checkout now</Button>
+              <CheckoutNow
+                currency="USD"
+                description={data.description}
+                image={data.images[0]}
+                name={data.name}
+                price={data.price}
+                key={data._id}
+                price_id={data.price_id}
+              />
             </div>
             <p className="mt-12 text-base text-gray-500 tracking-wide">
               {data.description}
